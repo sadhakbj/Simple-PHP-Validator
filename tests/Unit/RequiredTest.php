b9667e1ca9a2bc5681def9802aa1b62bdde2a786
Bijaya Prasad Kuikel <sadhakbj@gmail.com>
2023-12-29 13:11:16 +0900

    Added laravel pint for styling

--- tests/Unit/RequiredTest.php
+++ tests/Unit/RequiredTest.php
@@ -4,10 +4,10 @@ use Sadhakbj\Validator\Rules\Required;
 
 it('returns true is non empty value is passed', function () {
     $required = new Required();
-    expect($required->passes("Name", "Test Name"))->toBeTrue();
+    expect($required->passes('Name', 'Test Name'))->toBeTrue();
 });
 
 it('returns false is non empty value is passed', function () {
     $required = new Required();
-    expect($required->passes("Name", ""))->toBeFalse();
+    expect($required->passes('Name', ''))->toBeFalse();
 });
